11e6cfc2b7b63b5cc0f25c164a492cfd46f6b24b
Lawrence Lagerlof <llagerlof@gmail.com>
2022-07-20 14:31:22 -0300

    Unique directory name for each server instance
    - Included server path and data path in directory name.
    
    - Included system variables to backup.

--- mariabackup.php
+++ mariabackup.php
@@ -1,11 +1,34 @@
 <?php
-
 // Connect to MariaDB database using PDO
 try {
     $db = new PDO('mysql:host=localhost', 'root', '');
 } catch (Exception $e) {
     die("> Error connecting to database server.  (exception message: " . $e->getMessage() . ")\n");
 }
+
+// Get the @@GLOBAL.basedir and @@GLOBAL.basedir to use on directory name
+try {
+    $stmt = $db->query("select @@GLOBAL.basedir as basedir");
+} catch (Exception $e) {
+    die("> Error retrieving @@GLOBAL.basedir.  (exception message: " . $e->getMessage() . ")\n");
+}
+$basedir = $stmt->fetchColumn();
+
+// Get the @@GLOBAL.datadir and @@GLOBAL.datadir to use on directory name
+try {
+    $stmt = $db->query("select @@GLOBAL.datadir as datadir");
+} catch (Exception $e) {
+    die("> Error retrieving @@GLOBAL.datadir.  (exception message: " . $e->getMessage() . ")\n");
+}
+$datadir = $stmt->fetchColumn();
+
+// Get all system variables. Will be included in SYSTEM_VARIABLES.txt
+try {
+    $stmt = $db->query("show variables");
+} catch (Exception $e) {
+    die("> Error retrieving system variables.  (exception message: " . $e->getMessage() . ")\n");
+}
+$system_variables = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 // Get all database names
 try {
@@ -30,7 +53,7 @@ print_r($databases);
 
 echo "\n";
 
-$backup_dir = dirname(__FILE__) . '/dump';
+$backup_dir = getcwd() . '/backup-db_' . date('Y-m-d_H-i-s') . '_basedir[' . str2filename($basedir) . ']_datadir[' . str2filename($datadir) . ']';
 
 if (file_exists($backup_dir) && !is_dir($backup_dir)) {
     die("> Error: Directory $backup_dir could not be created because a file with the same name already exists.\n");
@@ -53,6 +76,11 @@ if (!empty(glob("$backup_dir/*.sql"))) {
     }
 }
 
+// Backup system variables
+echo "\n> Backuping system variables to SYSTEM_VARIABLES.txt... ";
+file_put_contents("$backup_dir/SYSTEM_VARIABLES.txt", print_r($system_variables, true));
+echo "done.\n";
+
 // Make a backup of each database in the list to a separate file using exec()
 foreach ($databases as $database) {
     echo "\n> Backuping database {$database} to $backup_dir... ";
@@ -61,4 +89,42 @@ foreach ($databases as $database) {
     echo "done.\n";
 }
 
-echo "Backup finished.\n";
+die ("Backup finished.\n");
+
+
+/* Utility functions */
+
+/**
+ * Convert any string to a valid filename
+ *
+ * @param string $str Any string
+ *
+ * @return string Valid and safe filename
+ */
+function str2filename(string $str): string
+{
+    // Replace ":\" with "."
+    $str = preg_replace('/:\\\/', '.', $str);
+    // Replace "\" with "."
+    $str = preg_replace('/\\\/', '.', $str);
+    // Replace "/" with "."
+    $str = preg_replace('/\//', '.', $str);
+    // Replace all spaces with underscores
+    $str = str_replace(' ', '_', $str);
+    // Replace all non alphanumeric characters, not including "-", "_" and ".", with dashes
+    $str = preg_replace('/[^A-Za-z0-9\-_\.]/', '-', $str);
+    // Replace a sequence of "." with one "."
+    $str = preg_replace('/\.+/', '.', $str);
+    // Replace a sequence of blank characters with one space
+    $str = preg_replace('/\s+/', ' ', $str);
+    // Replace a sequence of underscores with one underscore
+    $str = preg_replace('/_+/', '_', $str);
+    // Replace a sequence of hiphens with one hiphen
+    $str = preg_replace('/-+/', '-', $str);
+    // Remove all "-", "_" and "." from the end of the string
+    $str = preg_replace('/[\-_\.]+$/', '', $str);
+    // Remove all "-", "_" and "." from the start of the string
+    $str = preg_replace('/^[\-_\.]+/', '', $str);
+
+    return trim($str);
+}
